ac40cdc6c5b0559e68be2cb99c5af08bbb414807
Marco <marcoboncoraglio@gmail.com>
2018-10-13 03:06:03 +0200

    Added react equalizer in cardview

--- src/components/CardView/CardView.jsx
+++ src/components/CardView/CardView.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Grid, Col, Thumbnail } from 'react-bootstrap'
+import Equalizer from 'react-equalizer'
 import './CardView.css'
 
 const CardView = props => (
     <Grid bsClass="container nopadding">
-        {props.recipies.map((entry) => {
-            return (
-                <Col key={entry.recipe.uri} xs={12} md={3} bsClass="col">
-                    <Thumbnail bsClass="thumbnail" src={entry.recipe.image} alt={entry.recipe.label} href={entry.recipe.url}>
-                        <h4>{ entry.recipe.label }</h4>
-                        <p>by {entry.recipe.source}</p>
-                    </Thumbnail>                    
-                </Col>
-                )
+        <Equalizer>
+            {props.recipies.map((entry) => {
+                return (
+                    <Col key={entry.recipe.uri} xs={6} s={4} md={3} bsClass="col">
+                        <Thumbnail bsClass="thumbnail" src={entry.recipe.image} alt={entry.recipe.label} href={entry.recipe.url}>
+                            <h4>{ entry.recipe.label }</h4>
+                            <p>by {entry.recipe.source}</p>
+                        </Thumbnail>                    
+                    </Col>
+                    )
             })}
+        </Equalizer>
     </Grid>
 )
 
